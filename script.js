3a7b78c5be4e851afab78b9b609eb249a83abf35
BeingChilling <nauilnil95@gmail.com>
2023-07-04 23:03:31 +0200

    The finished calculator.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-// /02. Create class
+// /03. Create class
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
@@ -13,27 +13,108 @@ class Calculator {
     this.operation = undefined;
   }
 
-  delete() {}
-
-  appendNumber(number) {
-    this.currentOperand = number;
+  // /14.
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
-  chooseOperation(operation) {}
+  appendNumber(number) {
+    // /06. So we add in a check method:
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+    // /05. We append number to the end. But the dot also could be appended to the end many times.
+  }
 
-  compute() {}
+  chooseOperation(operation) {
+    // /10. We need to have a check in
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
+      this.compute();
+    }
+    // /08. When I click a operand, current things will be brought up to previous operand and current operand will be cleared.
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
+  }
+
+  // /12.
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
+  }
+
+  // /17.
+  getDisplayNumber(number) {
+    // /19.
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+
+    // /18.
+    // const floatNumber = parseFloat(number);
+    // if (isNaN(floatNumber)) return '';
+    // return floatNumber.toLocaleString('en');
+
+    // /20.
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
 
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    // /16.
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
+    // /09. Display the previous operand
+    // this.previousOperandTextElement.innerText = this.previousOperand;
   }
 }
 
-// /01. select everything.
-const numberButtons = document.querySelectorAll('[data-number');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equal]');
-const deleteButtons = document.querySelector('[data-delete]');
-const allClearButtons = document.querySelector('[data-all-clear]');
+// /02. select everything.
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operand]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector(
   '[data-previous-operand]'
 );
@@ -41,7 +122,7 @@ const currentOperandTextElement = document.querySelector(
   '[data-current-operand]'
 );
 
-// /03. The first function
+// /04. The first incident
 const calculator = new Calculator(
   previousOperandTextElement,
   currentOperandTextElement
@@ -52,4 +133,29 @@ numberButtons.forEach(button => {
     calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
   });
+});
+// /07.
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// /11.
+equalsButton.addEventListener('click', button => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+// /13.
+allClearButton.addEventListener('click', button => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+// /15.
+deleteButton.addEventListener('click', button => {
+  calculator.delete();
+  calculator.updateDisplay();
 });
